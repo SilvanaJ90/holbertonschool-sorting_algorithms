3df30b84d05db84e910eda4a678203346eb66c14
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-06 15:46:55 -0500

    error_betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,16 @@
 #include "sort.h"
 #include <time.h>
 
-void swap(int* xp, int* yp)
+/**
+ * swap - check code
+ * @xp: array
+ * @yp: tamapo
+ */
+void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
 
 void bubble_sort(int *array, size_t size)
@@ -19,17 +24,13 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 				/* Intercambiar  */
 				swap(&array[j], &array[j + 1]);
-
-	/*clock_t end = clock();*/
-	/*double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;*/
-
-	/*printf("Spent time: %f\n", time_spent);*/
 }
 
 void printArray(int arr[], int size)
 {
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d%s", arr[i], i + 1 == size ? "}" : ", ");
-    printf("\n");
+	int i;
+
+	for (i = 0; i < size; i++)
+		printf("%d%s", arr[i], i + 1 == size ? "}" : ", ");
+	printf("\n");
 }
