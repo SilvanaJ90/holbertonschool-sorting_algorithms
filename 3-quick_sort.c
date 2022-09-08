57df059155b1806c1b29a168a959707df7624bb3
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-08 12:59:58 -0500

    create-3-quicksort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,3 +1,4 @@
+
 #include "sort.h"
 
 /**
@@ -13,35 +14,53 @@ void swap(int *xp, int *yp)
 }
 
 /**
- * quick_sort - check code
+ * partition - check code
  * @array: array
- * @size: size to array
+ * @size: size array
  */
 
+int partition(int  *array, size_t size)
+{
+	int pivot;
+	size_t i = -1;
+	size_t j;
+
+	if (!array || size < 2)
+		return (0);
+
+	pivot = array[size - 1];
+
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != size - 1)
+	{
+		swap(&array[i + 1], &array[size - 1]);
+	}
+	return (i + 1);
+}
+/**
+ * quick_sort - check code
+ * @array: Array
+ * @size: Size of array
+ */
 void quick_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, ctral, first = 0, last = 0;
-	double pivot;
+	size_t pivot;
 
-	ctral = ( first + last) / 2;
-	pivot = array[ctral];
+	if (!array || size < 2)
+		return;
 
-	i = first;
-	j = size - 1;
-
-	do{
-		while (array[i] < pivot) i++;
-		while (array[j] > pivot) j--;
-
-		if (i <= j)
-		{
-			swap(&array[i], &array[j]);
-			i++;
-			j--;
-		}
-	}while (i <= j);
-	if (first < j)
-		quick_sort(array, first);
-	if (i < last)
-		quick_sort(array, last);
+	pivot = partition(array, size);
+	quick_sort(array, pivot);
+	quick_sort(array + pivot, size - pivot);
 }
